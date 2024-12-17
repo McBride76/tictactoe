27f582f22de7728d58b280f678d27777d076d3e0
Ian McBride <ianmcbride777@gmail.com>
2024-12-17 12:37:05 -0600

    Tile bg color changes on hover

--- public/js/game.js
+++ public/js/game.js
@@ -1,5 +1,6 @@
 "use strict";
 const radioButtons = document.querySelectorAll('input[name="playOption"]');
+const tiles = document.querySelectorAll('.tile');
 const TicTacToe = {
     mode: 'CPU',
     inMatch: false
@@ -9,4 +10,13 @@ radioButtons.forEach((radio) => {
         var _a;
         TicTacToe.mode = (_a = Array.from(radioButtons).find(radio => radio.checked)) === null || _a === void 0 ? void 0 : _a.value;
     });
+});
+tiles.forEach((tile) => {
+    tile.addEventListener('mouseover', (e) => {
+        let p = e.target;
+        if (p.innerText === "") {
+            tile.style.backgroundColor = '#fafcff';
+        }
+    });
+    tile.addEventListener('mouseout', (e) => tile.style.backgroundColor = '#ffffff');
 });
--- src/ts/game.ts
+++ src/ts/game.ts
@@ -1,4 +1,5 @@
 const radioButtons: NodeListOf<HTMLInputElement> = document.querySelectorAll('input[name="playOption"]')!;
+const tiles: NodeListOf<HTMLDivElement> = document.querySelectorAll('.tile')!;
 
 type PlayMode = 'CPU' | 'PVP';
 
@@ -16,4 +17,15 @@ radioButtons.forEach((radio: HTMLInputElement) => {
     radio.addEventListener('change', () => {
         TicTacToe.mode = Array.from(radioButtons).find(radio => radio.checked)?.value as PlayMode;
     })
+});
+
+tiles.forEach((tile: HTMLDivElement) => {
+    tile.addEventListener('mouseover', (e: MouseEvent) => {
+        let p = e.target as HTMLParagraphElement;
+        if (p.innerText === "") {
+            tile.style.backgroundColor = '#fafcff';
+        }
+    })
+
+    tile.addEventListener('mouseout', (e: MouseEvent) => tile.style.backgroundColor = '#ffffff');
 });
